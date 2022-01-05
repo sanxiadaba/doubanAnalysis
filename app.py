67e75be5a4ad7a6904af897ae6fa37286f13cdd5
jun <2190165626@qq.com>
2022-01-06 01:03:25 +0800

    增加了requirements

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__ ,template_folder='template')
 import pandas as pd
 n_row=250
+
+
 @app.route('/',methods=["GET"])
 def home():
     df=pd.read_excel('./Top250.xls')
@@ -11,6 +13,7 @@ def home():
     scores=list(movies["影片评分"][0:n_row])
     counts=list(movies["影片评价人数"][0:n_row])
     overviews=list(movies["影片概况"][0:n_row])
+    # print(names)
     return render_template("douban.html",n=list(range(1,n_row+1)),names=names,links=links,scores=scores,counts=counts,overviews=overviews)
 
 
